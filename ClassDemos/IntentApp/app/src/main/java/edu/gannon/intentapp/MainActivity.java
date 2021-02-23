5cc1f41365e3239e98577244e37a49d98c397b01
Richard Matovu <rkmatrich@gmail.com>
2021-02-23 09:04:08 -0500

    Class Demo - Intents

---
+++ ClassDemos/IntentApp/app/src/main/java/edu/gannon/intentapp/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.gannon.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ ClassDemos/IntentApp/app/src/main/java/edu/gannon/intentapp/SecondActivity.java
@@ -0,0 +1,16 @@
+package edu.gannon.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
